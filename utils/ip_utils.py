c0760632cb204ca6de6e845abceeda07897de1d6
Gabriel Gegenhuber <ggegenhuber@sba-research.org>
2024-04-04 15:21:38 +0200

    update iptools

--- utils/ip_utils.py
+++ utils/ip_utils.py
@@ -5,20 +5,17 @@ import requests.packages.urllib3.util.connection as urllib3_cn
 
 PREFERED_ADDR = socket.AF_UNSPEC
 
-def get_default_source_address(af_inet):
+def get_default_source_address(af_inet, prot=socket.SOCK_DGRAM, timeout=5):
     if af_inet == socket.AF_INET:
-        return [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+        return [(s. settimeout(timeout), s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, prot)]][0][2]
     elif af_inet == socket.AF_INET6:
-        return [(s.connect(('2001:4860:4860::8888', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)]][0][1]
+        return [(s. settimeout(timeout), s.connect(('2001:4860:4860::8888', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET6, prot)]][0][2]
     return None
 
+# check if the host has a (private) ipv6 addr
 def is_ipv6_supported():
     try:
-        ipv6_addr = get_default_source_address(socket.AF_INET6)
-        return ipv6_addr
-        # maybe check if addr is global? idk
-        #ipv6_addr = ip_address(ipv6_addr)
-        #return ipv6_addr.is_global        
+        return get_default_source_address(socket.AF_INET6, socket.SOCK_DGRAM) and get_default_source_address(socket.AF_INET6, socket.SOCK_STREAM) #sometimes DGRAM gets an (internal) address although we cannot access public ipv6 addrs...
     except:
         return False
 
